f39575c3387f5c44585ed9b5b5b44c9329aaa9ba
Musc0de <devmustest@gmail.com>
2025-02-13 08:39:49 -0800

    Delete FINAL_fixed_v.1.0 note_backup_v2.0 directory

--- FINAL_fixed_v.1.0 note_backup_v2.0/admin/update_last_activity.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (isset($_POST['update']) && $_POST['update'] === 'true') {
-    $_SESSION['last_activity'] = time();
-    echo 'Activity updated';
-}
-?>
--- FINAL_fixed_v.1.0 note_backup_v2.0/api/set_session.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$note_path_safe = $_POST['note_path'] ?? '';
-$status = $_POST['status'] ?? ''; // Tambahkan untuk menerima status
-
-if (!empty($note_path_safe)) {
-    $_SESSION['authenticated_notes'][$note_path_safe] = $status; // Simpan status yang dikirim
-}
